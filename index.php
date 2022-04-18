15151edab81825f49947e3cf8e8f8c0c11d2f2f0
MahmoodNagy <mahmoud.development94@gmail.com>
2022-04-18 22:16:01 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
 <?php include("index.html"); ?>
+
+<?php
+$hostName = "127.0.0.1";
+$userName = "root";
+$password = "Nagy@1994";
+$databaseName = "accelgame_db";
+ $conn = new mysqli($hostName, $userName, $password, $databaseName);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+<?php
+$host    = "127.0.0.1";
+$user    = "root";
+$pass    = "Nagy@1994";
+$db_name = "accelgame_db";
+
+//create connection
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$connection = mysqli_connect($host, $user, $pass, $db_name);
+
+//get results from database
+$result = mysqli_query($connection, "SELECT * FROM current_winners");
+$all_property = array();  //declare an array for saving property
+
+//showing property
+echo '<table class="data-table">
+        <tr class="data-heading">';  //initialize table tag
+while ($property = mysqli_fetch_field($result)) {
+    echo '<td>' . $property->name . '</td>';  //get field name for header
+    $all_property[] = $property->name;  //save those to array
+}
+echo '</tr>'; //end tr tag
+
+//showing all data
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    foreach ($all_property as $item) {
+        echo '<td>' . $row[$item] . '</td>'; //get items using property value
+    }
+    echo '</tr>';
+}
+echo "</table>";
+?>
